e6749483385f056a70fa9b67bd9c3f9349007e6d
Monta <zouaouimonta12345@gmail.com>
2023-10-15 12:27:55 +0100

    Exercice 1 done

---
+++ somme.java
@@ -0,0 +1,15 @@
+public class somme{
+    public static void main(String[] args) {
+        int s=0;
+        for (int i = 0; i < 100; i++) {
+            s+=i;
+            
+        }
+        System.out.println("somme = "+s);
+    }
+
+
+
+
+
+}
