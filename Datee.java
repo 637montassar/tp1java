eb6ad4a276d61046e82f94d8516bc7f97a39a0df
Monta <zouaouimonta12345@gmail.com>
2023-10-29 12:38:40 +0100

    Exercice 9 done

--- Datee.java
+++ Datee.java
@@ -10,32 +10,41 @@ public class Datee {
     }
     @Override
     public String toString(){
-        return (jour+" / "+mois+" / "+" annee ") ;
+        return (jour+" / "+mois+" / "+annee) ;
     }
     public boolean bissex(){
         return ( (annee % 4==0) && !(annee%100==0) || (annee % 400 == 0) ); 
 
     }
     public int nbJour(int mois) {
-        int s=0;
+        int s = 0;
         switch (mois) {
-            case 1, 3, 5, 7, 8, 10, 12:
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
                 s = 31;
                 break;
-            case 4, 6, 9, 11:
+            case 4:
+            case 6:
+            case 9:
+            case 11:
                 s = 30;
                 break;
             case 2:
                 if (bissex()) {
                     s = 29;
-                } 
-                else {
+                } else {
                     s = 28;
                 }
                 break;
         }
         return s;
     }
+    
     
     public int nbJoursEcoules(){
         int s=0;
@@ -58,7 +67,51 @@ public class Datee {
 
     }
     public static long nbJoursEntre(Datee d1, Datee d2){
-        
+        long s=0;
+        if ( d1.annee < d2.annee){
+            for (int i=d1.annee;i<d2.annee-1;i++){
+                if (bissex(i)){
+                    s=s+366;
+                }
+                else{
+                    s=s+365;
+                }
+            }
+            s=s+d1.nbJoursRestants();
+            s=s+d2.nbJoursEcoules();
+
+        }
+        else if (d1.annee > d2.annee){
+            for (int i=d2.annee;i<d1.annee-1;i++){
+                if (bissex(i)){
+                    s=s+366;
+                }
+                else{
+                    s=s+365;
+                }
+            }
+            s=s+d2.nbJoursRestants();
+            s=s+d1.nbJoursEcoules();
+        }
+        else{
+            if (d1.mois>d2.mois){
+                
+                s = 365 - (d1.nbJoursRestants() + d2.nbJoursEcoules() );
+                if (bissex(d1.annee)){ s=s+1; }  // less complicated : s = d2.nbJoursEcoules() - d1.nbJoursEcoules();
+            
+            }
+            else if(d2.mois>d1.mois){
+
+                s = 365 - (d2.nbJoursRestants() + d1.nbJoursEcoules() );
+                if (bissex(d1.annee)){ s=s+1; } // less complicated : s = d1.nbJoursEcoules() - d2.nbJoursEcoules();
+            }
+            else{
+                s = Math.abs(d2.jour - d1.jour);
+            }
+
+
+        }
+        return s;
     }
 
     
--- EssaiDate.java
+++ EssaiDate.java
@@ -1,9 +1,17 @@
 public class EssaiDate {
     public static void main(String[] args) {
-        Datee d = new Datee(3,2,200);
-        System.out.println(d.bissex());
+        System.out.println(Datee.bissex(1900));
+        System.out.println(Datee.bissex(1920));
+        System.out.println(Datee.bissex(1989));
+        System.out.println(Datee.bissex(2000));
+        System.out.println(Datee.bissex(2003));
+        Datee d= new Datee(3, 2, 2003);
+        System.out.println("au " +d);
         System.out.println(d.nbJoursEcoules());
         System.out.println(d.nbJoursRestants());
+        System.out.println(Datee.nbJoursEntre(new Datee(1, 1, 1900),new Datee(1, 1, 2000)));
+
+
     }
     
 }
